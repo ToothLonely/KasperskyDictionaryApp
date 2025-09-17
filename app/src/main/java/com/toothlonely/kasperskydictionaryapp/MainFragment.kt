24250a26239668fd3586dbd156b0c618b1037acb
ToothLonely <arsen.shirinov2004@gmail.com>
2025-09-18 00:57:40 +0300

    KD-1 added MainFragment

---
+++ app/src/main/java/com/toothlonely/kasperskydictionaryapp/MainFragment.kt
@@ -0,0 +1,26 @@
+package com.toothlonely.kasperskydictionaryapp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.toothlonely.kasperskydictionaryapp.databinding.FragmentMainBinding
+
+class MainFragment() : Fragment() {
+
+    private var _mainFragmentBinding: FragmentMainBinding? = null
+    private val mainFragmentMainBinding: FragmentMainBinding
+        get() = _mainFragmentBinding ?: throw IllegalStateException(
+            "Binding for CategoriesListFragmentBinding mustn't be null"
+        )
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _mainFragmentBinding = FragmentMainBinding.inflate(inflater, container, false)
+        return mainFragmentMainBinding.root
+    }
+}
