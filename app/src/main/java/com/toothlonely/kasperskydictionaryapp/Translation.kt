ecadf7ce53e3873f9a375b13ad8f26979785ccec
ToothLonely <arsen.shirinov2004@gmail.com>
2025-09-19 16:05:21 +0300

    KD-9 little fixes

--- app/src/main/java/com/toothlonely/kasperskydictionaryapp/Translation.kt
+++
@@ -1,6 +0,0 @@
-package com.toothlonely.kasperskydictionaryapp
-
-data class Translation(
-    val original: String,
-    val translate: String,
-)
--- app/src/main/java/com/toothlonely/kasperskydictionaryapp/WordsAPIService.kt
+++ app/src/main/java/com/toothlonely/kasperskydictionaryapp/WordsAPIService.kt
@@ -5,10 +5,6 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface WordsAPIService {
-
-    @GET("meanings")
-    fun getMeanings(@Query("ids") ids: String): Call<List<Meaning>>
-
     @GET("words/search")
     fun getWords(@Query("search") word: String): Call<List<Word>>
 }
